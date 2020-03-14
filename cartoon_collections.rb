8669df4cf0445a445bf9d810d522db22e8ce3455
Monica Gerard <mlgerardvla@gmail.com>
2020-03-14 03:16:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 def long_planeteer_calls(array)
   collection = []
   collection << array.collect {|word| word.length > 4}
-  collection.includes?(true)? true : false
+  collection.include?(true)? true : false
 end
 
 def find_the_cheese# code an argument here
@@ -19,4 +19,4 @@ def find_the_cheese# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
 
-long_planeteer_calls(["wind", "Monica", "tree", "axe", "code"])
+# long_planeteer_calls(["wind", "Monica", "tree", "axe", "code"])
