f971d64e6ea6e297dd681d80e607259fc26727bc
Monica Gerard <mlgerardvla@gmail.com>
2020-03-14 03:09:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def roll_call_dwarves(array)
   array.each_with_index {|item, index| puts "#{index + 1}. #{item}"}
 end
@@ -8,8 +10,9 @@ end
 
 def long_planeteer_calls(array)
   collection = []
+  binding.pry
   collection << array.collect {|word| word.length > 4}
-  puts collection
+
   if collection.size == 0
     return false
   end
@@ -20,3 +23,5 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+long_planeteer_calls("wind", "fire", "tree", "axe", "code")
