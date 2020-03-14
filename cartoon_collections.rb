4c139658a44310e78d0eb152fcec1935551866ae
Monica Gerard <mlgerardvla@gmail.com>
2020-03-14 04:14:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,15 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_types.each do |cheese|
+    if array.include?(cheese)
+      cheese
+    else
+      nil
+    end  
+  end
 end
