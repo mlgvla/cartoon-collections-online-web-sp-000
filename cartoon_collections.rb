a699f38d290b225609a5ce7fd60db5bbd349f489
Monica Gerard <mlgerardvla@gmail.com>
2020-03-14 03:17:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 def long_planeteer_calls(array)
   collection = []
   collection << array.collect {|word| word.length > 4}
-  collection.include?(true)? true : false
+  if collection.include?
 end
 
 def find_the_cheese# code an argument here
