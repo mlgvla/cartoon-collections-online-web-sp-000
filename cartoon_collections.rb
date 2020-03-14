1f512383927baa95e9b846cbf0ecb37cb52854e9
Monica Gerard <mlgerardvla@gmail.com>
2020-03-14 02:38:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.find {|word| word.length > 4 ? true : false}
+  if array.find {|word| word.length > 4} == nil
+    false
+  true
 end
 
 def find_the_cheese# code an argument here
